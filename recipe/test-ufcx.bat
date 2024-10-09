c3d57acb68ea1c406f790c133fecb201d990c7c8
Min RK <benjaminrk@gmail.com>
2024-10-09 15:20:42 +0200

    add Windows builds

---
+++ recipe/test-ufcx.bat
@@ -0,0 +1,19 @@
+setlocal EnableDelayedExpansion
+
+cmake -G "NMake Makefiles" ^
+  -B build-dir ^
+  -DCMAKE_BUILD_TYPE=Release ^
+  -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
+  -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+  test-ufcx
+
+if errorlevel 1 exit 1
+
+cmake --build build-dir --verbose
+if errorlevel 1 exit 1
+
+cmake --install build-dir
+if errorlevel 1 exit 1
+
+test-ufcx
+if errorlevel 1 exit 1
